cd8b472d4c96675f081ac00d27fdf2cd599ae63e
psczlek <cyprian_ccp@proton.me>
2025-07-29 00:19:19 +0200

    fix coloring on non-truecolor terminals

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,57 @@
-use colored::*;
 use std::{env, fmt::Display};
 
+use colored::{Colorize, CustomColor};
+
 fn main() {
-    let envars = env::vars();
-    for (key, value) in envars {
+    for (key, value) in env::vars() {
         let line = Line::new("==>", &key, "=", &value);
         println!("{line}");
     }
 }
 
-struct Line {
-    precedence: String,
-    key: String,
-    sep: String,
-    value: String,
+struct Line<'a> {
+    precedence: &'a str,
+    key: &'a str,
+    sep: &'a str,
+    value: &'a str,
+    is_truecolor_term: bool,
 }
 
-impl Line {
-    fn new(precedence: &str, key: &str, sep: &str, value: &str) -> Self {
+impl<'a> Line<'a> {
+    fn new(precedence: &'a str, key: &'a str, sep: &'a str, value: &'a str) -> Self {
         Self {
-            precedence: String::from(precedence),
-            key: String::from(key),
-            sep: String::from(sep),
-            value: String::from(value),
+            precedence: precedence,
+            key: key,
+            sep: sep,
+            value: value,
+            is_truecolor_term: env::var("COLORTERM")
+                .map(|colorterm| colorterm == "truecolor" || colorterm == "24bit")
+                .unwrap_or(false),
         }
     }
 }
 
-impl Display for Line {
+impl<'a> Display for Line<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{} {}{}{}",
-            self.precedence
-                .custom_color(CustomColor::new(110, 106, 241))
-                .bold(),
-            self.key.custom_color(CustomColor::new(42, 195, 222)),
-            self.sep.custom_color(CustomColor::new(160, 160, 160)),
-            self.value.custom_color(CustomColor::new(241, 163, 111)),
-        )
+        match self.is_truecolor_term {
+            false => write!(
+                f,
+                "{} {}{}{}",
+                self.precedence.blue().bold(),
+                self.key.cyan(),
+                self.sep,
+                self.value.yellow(),
+            ),
+            true => write!(
+                f,
+                "{} {}{}{}",
+                self.precedence
+                    .custom_color(CustomColor::new(110, 106, 241))
+                    .bold(),
+                self.key.custom_color(CustomColor::new(42, 195, 222)),
+                self.sep.custom_color(CustomColor::new(160, 160, 160)),
+                self.value.custom_color(CustomColor::new(241, 163, 111)),
+            ),
+        }
     }
 }
